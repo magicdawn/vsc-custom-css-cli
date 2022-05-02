b7434c1ba9b92541cf430a05d83c8d441d231580
magicdawn <magicdawn@qq.com>
2022-05-02 18:23:33 +0800

    chore: fix husky

--- src/cli.ts
+++ src/cli.ts
@@ -16,6 +16,7 @@ cli.register(Builtins.DefinitionsCommand)
 // more commands
 import AddCommand from './commands/add'
 cli.register(AddCommand)
+
 import ListCommand from './commands/list'
 cli.register(ListCommand)
 
